c4607cd819e0283402a81ae6e35debccc3da7d78
Alejandro Garcia <garciaalejandro2552@gmail.com>
2025-05-03 12:15:24 -0500

    Init function, class DirectoryCleaner

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,10 @@
 import os
+import shutil
+import time
+import datetime
+import logging
+from pathlib import Path
+import re
 
 
 class DirectoryCleaner: 
@@ -14,7 +20,7 @@ class DirectoryCleaner:
 
     def __init__(self, target_directory=None, archive_base_dir=None):
         """
-        Intializes Directory cleaner
+        Intializes DirectoryCleaner
 
         Args:
             target_directory (str): Directory to be cleaned up, By default is Downloads directory.
@@ -33,3 +39,35 @@ class DirectoryCleaner:
         else:    
             self.archive_base_dir = archive_base_dir
 
+
+        #Ensure archive file exists
+        os.makedirs(self.archive_base_dir, exist_ok=True)
+
+        #Logging
+        self._setup_logging()
+
+
+        """ Processing RUles"""
+
+        self.rules = [{
+            '.csv':'archive',
+            '.txt':'archive',
+            '.xml':'archive',
+            '.pdf':'delete',
+            '.xls':'delete',
+            '.xlsx':'delete',
+        }]
+
+        
+        self.name_rules= [{
+            'pattern': re.compile(r'EPAY', re.IGNORECASE),
+            'action': 'move',
+            'destination': os.path.join(self.archive_base_dir, 'EPAY')
+        }]
+
+        for rule in self.name_rules:
+                if rule['action'] == 'move':
+                     os.makedirs(rule['destination'], exist_ok=True)
+                     
+
+    
